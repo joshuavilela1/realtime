a9bfef7ca6e510fc9da75bd6a3f7a8d105c34ecd
joshuavilela <joshuavilela1@gmail.com>
2023-10-25 14:40:31 -0700

    Added RequestAnimationFrame instead of long polling

--- polling/backoff-and-retry/frontend/polling-chat.js
+++ polling/backoff-and-retry/frontend/polling-chat.js
@@ -1,14 +1,14 @@
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
+const chat = document.getElementById('chat');
+const msgs = document.getElementById('msgs');
 
 let allChat = [];
 
 const INTERVAL = 3000;
 
-chat.addEventListener("submit", function (e) {
+chat.addEventListener('submit', function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+  chat.elements.text.value = '';
 });
 
 async function postNewMsg(user, text) {
@@ -19,24 +19,24 @@ async function postNewMsg(user, text) {
 
   // request options
   const options = {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   };
 
   // send POST request
-  await fetch("/poll", options);
+  await fetch('/poll', options);
 }
 
 async function getNewMsgs() {
   try {
-    const res = await fetch("/poll");
+    const res = await fetch('/poll');
     const json = await res.json();
 
     if (res.status >= 400) {
-      throw new Error("request did not succeed: " + res.status);
+      throw new Error('request did not succeed: ' + res.status);
     }
 
     allChat = json.msg;
@@ -50,7 +50,7 @@ async function getNewMsgs() {
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+  msgs.innerHTML = html.join('\n');
 }
 
 const template = (user, msg) =>
@@ -59,11 +59,13 @@ const template = (user, msg) =>
 const BACKOFF = 5000;
 let timeToMakeNextRequest = 0;
 let failedTries = 0;
+
 async function rafTimer(time) {
   if (timeToMakeNextRequest <= time) {
     await getNewMsgs();
     timeToMakeNextRequest = time + INTERVAL + failedTries * BACKOFF;
   }
+
   requestAnimationFrame(rafTimer);
 }
 
