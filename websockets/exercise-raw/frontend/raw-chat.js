6c394182bcc0d252e65031acb237abaab8290a6c
joshuavilela <joshuavilela1@gmail.com>
2023-10-27 00:01:48 -0700

    Introduced http2

--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -1,13 +1,13 @@
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
-const presence = document.getElementById("presence-indicator");
+const chat = document.getElementById('chat');
+const msgs = document.getElementById('msgs');
+const presence = document.getElementById('presence-indicator');
 let allChat = [];
 
 // listen for events on the form
-chat.addEventListener("submit", function (e) {
+chat.addEventListener('submit', function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+  chat.elements.text.value = '';
 });
 
 async function postNewMsg(user, text) {
@@ -16,13 +16,13 @@ async function postNewMsg(user, text) {
 
 /*
  *
- * your code goes here
+ * your code goes here!
  *
  */
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+  msgs.innerHTML = html.join('\n');
 }
 
 const template = (user, msg) =>
