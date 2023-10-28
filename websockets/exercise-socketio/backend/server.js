8bf61351e019a902654f2f4af9fdb98758ea4ac2
joshuavilela <joshuavilela1@gmail.com>
2023-10-28 14:53:24 -0700

    Introduced websockets/socket.io

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -1,31 +1,35 @@
-import http from "http";
-import handler from "serve-handler";
-import nanobuffer from "nanobuffer";
-import { Server } from "socket.io";
+import http from 'http';
+import handler from 'serve-handler';
+import nanobuffer from 'nanobuffer';
+import { Server } from 'socket.io';
 
 const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
+  user: 'brian',
+  text: 'hi',
   time: Date.now(),
 });
 
 // serve static assets
 const server = http.createServer((request, response) => {
   return handler(request, response, {
-    public: "./frontend",
+    public: './frontend',
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+const io = new Server(server, {});
+
+io.on('connection', (socket) => {
+  console.log(`connected: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`disconnected: ${socket.id}`);
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at http://localhost:${port}`),
 );
--- websockets/exercise-socketio/frontend/socketio-chat.js
+++ websockets/exercise-socketio/frontend/socketio-chat.js
@@ -1,20 +1,25 @@
 // a global called "io" is being loaded separately
 
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
-const presence = document.getElementById("presence-indicator");
+const chat = document.getElementById('chat');
+const msgs = document.getElementById('msgs');
+const presence = document.getElementById('presence-indicator');
 let allChat = [];
 
-/*
- *
- * Code goes here
- *
- */
+const socket = io('http://localhost:8080');
 
-chat.addEventListener("submit", function (e) {
+socket.on('connect', () => {
+  console.log('connected');
+  presence.innerText = '🟢';
+});
+
+socket.on('disconnect', () => {
+  presence.innerText = '🔴';
+});
+
+chat.addEventListener('submit', function (e) {
   e.preventDefault();
   postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+  chat.elements.text.value = '';
 });
 
 async function postNewMsg(user, text) {
@@ -27,7 +32,7 @@ async function postNewMsg(user, text) {
 
 function render() {
   const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+  msgs.innerHTML = html.join('\n');
 }
 
 const template = (user, msg) =>
